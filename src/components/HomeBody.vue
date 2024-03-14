ee521c33d7347ec5e8bd2c6b8dc2f8c5fe580c6e
micapremat <micapremat@gmail.com>
2024-03-14 13:29:01 +0100

    runner - addeventlistener - removeventlistener

--- src/components/HomeBody.vue
+++ src/components/HomeBody.vue
@@ -1,19 +1,78 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const isRunning = ref(false)
+
+const handleKeyPress = (e: KeyboardEvent) => {
+    //get runner image
+    let runner = document.getElementById("runner")
+    //get runner image position
+    let runnerX = runner?.offsetLeft || 0
+    
+    //get finish image
+    let finish = document.getElementById("finish")
+    //get finish image position minus it width
+    let finishX = 0
+    if(finish !== null) {
+         finishX = finish.offsetLeft - finish.offsetWidth
+    }
+
+    if(runner !== null) {
+        if(e.key == 'ArrowRight') {
+            runnerX+=10
+            //add the position to the runner image
+            runner.style.left =  `${runnerX}px` 
+            //when runner position mayor to finish position go to about page
+            if (finishX <= runnerX) {
+                runnerX = 0
+                document.removeEventListener("keyup", handleKeyPress)
+                router.push("/about")
+            }
+        } else if(e.key == 'ArrowLeft') {
+            runnerX-=10
+            //add the position to the runner image
+            runner.style.left =  `${runnerX}px` 
+        }
+    }
+};  
+
+const followme = () => {
+    isRunning.value = true
+    
+    //start to move the runner when key up
+    document.addEventListener("keyup", handleKeyPress)
+}
+
+const stopRunner = () => {
+    isRunning.value = false;
+    document.removeEventListener("keyup", handleKeyPress)
+}
 </script>
 
 <template>
-  <div class="flex justify-around flex-row mt-14">
+  <div class="lg:flex justify-around flex-row mt-14">
     <div class="flex-col">
         <div class="text-center">
             <h3 class="text-xl text-primary font-medium"> Do you want to know me better?</h3>
         </div>
-        <div class="text-center">
-            <p class="underline font-semibold text-xl py-4">Follow me</p>
+        <div class="text-center mb-2">
+            <p class="underline font-semibold text-xl py-4 cursor-pointer" @click="followme">Go for it</p>
+            <p class="w-2/3 mx-auto font-bold text-sm">You just need to guide the runner to the finish line </p>
         </div>
-        <div>
-            <img src="../assets/img/follow-me.png" class="block mx-auto header-img w-10"/>
+        <div class="flex flex-row justify-around">
+            <div class=" self-end">
+                <img src="../assets/img/run.gif" class="absolute left-[20px] sm:left-[10%] lg:left-[15%] header-img w-20 cursor-pointer" id="runner" @click="followme"/>
+            </div>
+            <div class=" self-start">
+                <img src="../assets/img/finish.png" class="absolute right-[20px] sm:right-[15%] lg:left-[40%]  header-img w-6 cursor-pointer" id="finish"/>
+            </div>
         </div>
-        <div class="text-center py-4">
+        <div class="text-center" v-if="isRunning">
+            <button class=" text-red-600 font-semibold" @click="stopRunner">Stop the runner</button>
+        </div>
+        <div class="text-center py-4 mt-20">
             <p class="w-2/3 mx-auto font-bold text-lg">I will tell you about who I am, what I do, what I like, where I go... </p>
         </div>
     </div>
